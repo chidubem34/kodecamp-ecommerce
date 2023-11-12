72ab83a6bf88815a0ced5f29192fa846a31f4b72
chidubem34 <kizito65623281@gmail.com>
2023-11-12 21:28:27 +0200

    ch: login and signup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -44,7 +44,7 @@ const Login = () => {
 
                                     <div className='text-white bg-blue-700 cursor-pointer hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 w-20 font-medium rounded-lg text-sm px-5 py-2.5 text-center'>Login</div>
 
-                                    <p className='mt-3 text-sm font-medium text-gray-900'>Dont have an account? <Link className='text-blue-500' to={"/kodecamp-ecommerce/"}>register</Link> instead </p>
+                                    <p className='mt-3 text-sm font-medium text-gray-900'>Dont have an account? <Link className='text-blue-500' to={"/kodecamp-ecommerce/SignUP"}>register</Link> instead </p>
                                 </form>
                             </div>
                         </div>
